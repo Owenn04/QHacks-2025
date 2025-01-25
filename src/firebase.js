8b2e09c64b6e1ca54130a88f597214d0cb0b1a2b
froggy <xavawa@gmail.com>
2025-01-24 23:23:13 -0500

    firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,18 @@
 import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  // Your config from Firebase Console
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
+  measurementId: process.env.REACT_APP_measurementId,
 };
+
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
